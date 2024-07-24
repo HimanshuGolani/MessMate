e35c50da68d8400803dbaa904cafe7cddfefd015
HimanshuGolani <golanihimanshu@gmail.com>
2024-07-24 11:10:20 +0530

    vendor profile stage-1 added.

--- MessMate-Frontend/src/vendor-pages/VendorProfile.jsx
+++ MessMate-Frontend/src/vendor-pages/VendorProfile.jsx
@@ -1,28 +1,49 @@
+import React from "react";
 import { Button } from "@mui/material";
-import "./VendorProfile.css";
 import EditIcon from "@mui/icons-material/Edit";
+import "./VendorProfile.css";
 
-const VendorProfile = () => {
-  const handleEdit = async () => {};
+const VendorProfile = ({ vendorData }) => {
+  const handleEdit = async () => {
+    // Edit functionality
+  };
+
   return (
-    <div>
-      <div className="profile-container">
-        <header className="header">
-          <div className="heading">
-            <h1>Vendor Profile</h1>
-            <Button onClick={handleEdit}>
-              <EditIcon
-                sx={{
-                  color: " rgb(207, 126, 27)",
-                }}
-              />
-            </Button>
-          </div>
-        </header>
-        <main>
-          <div className="vendor-content"></div>
-        </main>
-      </div>
+    <div className="profile-container">
+      <header className="header">
+        <div className="heading">
+          <h1>Vendor Profile</h1>
+          <Button onClick={handleEdit}>
+            <EditIcon
+              sx={{
+                color: "rgb(207, 126, 27)",
+              }}
+            />
+          </Button>
+        </div>
+      </header>
+      <main>
+        <div className="vendor-content">
+          <ol>
+            <li>
+              <strong>Name of the Vendor:</strong> {"name"}
+            </li>
+            <li>
+              <strong>Number of Customers:</strong> {"customers"}
+            </li>
+            <li>
+              <strong>Number of Plans Offered:</strong> {"plans"}
+            </li>
+
+            <li>
+              <strong>Phone Number:</strong> {"phone"}
+            </li>
+            <li>
+              <strong>Address of the Mess:</strong> {"address"}
+            </li>
+          </ol>
+        </div>
+      </main>
     </div>
   );
 };
